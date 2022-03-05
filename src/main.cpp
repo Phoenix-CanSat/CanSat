eca6c0f4b7528d86315f11257d2f1f634d94a12c
Dimitris Dafermos <dimitrisjim321@gmail.com>
2022-03-05 17:22:28 +0200

    Bob now uses falling velocity for buzzer init/tion

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,18 @@
 // Sets minimum time between each loop.
 #define loopDelay 250
 
-// Estimated time needed for landing.
-#define landingTime 20000
+#define descentSamples 12
+#define stillSamples 4
 
 // Set buzzer beeping.
 #define beepDuration 1000
 #define beep() tone(4, 2000, beepDuration)
 
+float lastAltitude = 0;
+uint32_t lastTime = 0;
+uint8_t descend = 0;
+bool isDescending = false;
+uint8_t still = 0;
 bool buzzer = false;
 uint32_t lastBeep = 0;
 
@@ -79,14 +84,38 @@ void loop() {
 
 //-------------------------------------------------------Start Buzzer (If Landed)-------------------------------------------------------//
 
-    // When landed, sends message and starts the buzzer.
-    if (buzzer==false && Time()>=landingTime) {
-        Say("Bob has landed.\n");
-        buzzer = true;
+    // Checks if buzzer is already on.
+    if (buzzer == false) {
+        // Calculates descending speed.
+        float velocity = (altitude - lastAltitude) / (float)(time - lastTime);
+
+        // Checks if Bob is descending.
+        if (isDescending == false) {
+            if (velocity < 0 && descend < descentSamples) {
+                descend += 1;
+            } else if (velocity >= 0 && descend < descentSamples) {
+                descend = 0;
+            } else {
+                isDescending = true;
+            }
+        } else {
+            // When landed, sends message and starts the buzzer.
+            if (still < stillSamples && abs(velocity) < 1) {
+                still += 1;
+            } else if (still < stillSamples && abs(velocity) >= 1) {
+                still = 0;
+            } else if (still == stillSamples && abs(velocity) < 1) {
+                Say("Bob has landed.\n");
+                buzzer = true;
+            }
+        }
+
+        lastAltitude = altitude;
+        lastTime = time;
     }
 
     // After landing, beep in set intervals.
-    if (buzzer==true && Time()-lastBeep>=2*beepDuration) {
+    if (buzzer == true && Time() - lastBeep >= 2 * beepDuration) {
         beep();
         lastBeep = Time();
     }
@@ -104,7 +133,6 @@ void loop() {
 /// TODO:
 ///     Test GPS with battery.
 ///     Program Flight Controller (Betaflight).
-///     Use speed of descendance for initializing the buzzer.
 ///     Create SD support for Ground Station.
 ///     Add Identifier with Packet Counter before every packet.
 ///     Add support for Packet Identifiers in Ground Station.
